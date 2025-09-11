3f234f2c6cf63dc5e04dd51dbc8bfe032cf4a02b
luisfsantos22 <luisfsantos@ua.pt>
2025-09-11 04:01:47 +0100

    fix: remove commented-out authentication cookie checks and improve login redirection logic in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,23 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
-export function middleware(request: NextRequest) {
+// const AUTH_COOKIES = [
+//   'next-auth.session-token',
+//   '__Secure-next-auth.session-token',
+// ]
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request })
   const { pathname } = request.nextUrl
   const isAuthPage = pathname.startsWith('/auth')
 
-  // Check if any auth cookie exists
+  if (!token && (request.nextUrl.pathname === '/' || !isAuthPage)) {
+    // Redirect to login if not authenticated and accessing root or non-auth pages
+    return NextResponse.redirect(new URL('/auth/signin', request.url))
+  }
+
+  // // Check if any auth cookie exists
   // const hasAuthCookie = AUTH_COOKIES.some((cookie) =>
   //   request.cookies.has(cookie)
   // )
