022c8270d4e8da43f29e511a5c0f4282a2b00225
luisfsantos22 <luisfsantos@ua.pt>
2025-09-11 04:08:56 +0100

    fix: streamline authentication logic in middleware by removing unused token checks and simplifying cookie validation

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,25 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getToken } from 'next-auth/jwt'
 
-// const AUTH_COOKIES = [
-//   'next-auth.session-token',
-//   '__Secure-next-auth.session-token',
-// ]
+const AUTH_COOKIES = ['sessionid']
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request })
+export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const isAuthPage = pathname.startsWith('/auth')
 
-  if (!token && (request.nextUrl.pathname === '/' || !isAuthPage)) {
-    // Redirect to login if not authenticated and accessing root or non-auth pages
-    return NextResponse.redirect(new URL('/auth/signin', request.url))
-  }
+  // Check if Django sessionid cookie exists
+  const hasAuthCookie = AUTH_COOKIES.some((cookie) =>
+    request.cookies.has(cookie)
+  )
 
-  if (token) {
-    try {
-      const accessToken = token.accessToken as string
-      if (!accessToken) {
-        return NextResponse.redirect(new URL('/auth/signin', request.url))
-      }
-    } catch (error) {
-      console.error('Error validating token:', error)
-
+  // 1. If not authenticated
+  if (!hasAuthCookie) {
+    if (!isAuthPage) {
       return NextResponse.redirect(new URL('/auth/signin', request.url))
     }
+
+    return NextResponse.next()
   }
-
-  // // Check if any auth cookie exists
-  // const hasAuthCookie = AUTH_COOKIES.some((cookie) =>
-  //   request.cookies.has(cookie)
-  // )
-
-  // // 1. If not authenticated
-  // if (!hasAuthCookie) {
-  //   if (!isAuthPage) {
-  //     return NextResponse.redirect(new URL('/auth/signin', request.url))
-  //   }
-
-  //   return NextResponse.next()
-  // }
 
   // 2. Prevent logged-in users from visiting /auth/*
   if (isAuthPage || pathname === '/') {
